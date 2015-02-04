524b1c6ecd15ca4ffb9241f425c3d018101adfe6
KletusVandamme <matt.hill.email@gmail.com>
2015-02-04 14:22:28 -0800

    show page working with seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,30 @@
-def create_robot!()
+def create_robot!(i)
   Robot.create!(
-    name: 'robot' + SecureRandom.urlsafe_base64(1).to_s,
+    name: 'robot' + i.to_s,
     manufacturer: 'Roboco',
-    image_url: %w(
-      http://placekitten.com/200/300?image=1
-      http://placekitten.com/200/300?image=2
-      http://placekitten.com/200/300?image=3
-      http://placekitten.com/200/300?image=4
-      http://placekitten.com/200/300?image=5
-      http://placekitten.com/200/300?image=6
-      http://placekitten.com/200/300?image=7
-      http://placekitten.com/200/300?image=8
-      http://placekitten.com/200/300?image=9
-      http://placekitten.com/200/300?image=10
-      http://placekitten.com/200/300?image=11
-      http://placekitten.com/200/300?image=12
-      http://placekitten.com/200/300?image=13
-      http://placekitten.com/200/300?image=14
-      http://placekitten.com/200/300?image=15
-      http://placekitten.com/200/300?image=16
-    ).sample
+    image_url: "robot" + i.to_s + ".png"
+
+    # image_url: %w(
+    #   http://placekitten.com/200/300?image=1
+    #   http://placekitten.com/200/300?image=2
+    #   http://placekitten.com/200/300?image=3
+    #   http://placekitten.com/200/300?image=4
+    #   http://placekitten.com/200/300?image=5
+    #   http://placekitten.com/200/300?image=6
+    #   http://placekitten.com/200/300?image=7
+    #   http://placekitten.com/200/300?image=8
+    #   http://placekitten.com/200/300?image=9
+    #   http://placekitten.com/200/300?image=10
+    #   http://placekitten.com/200/300?image=11
+    #   http://placekitten.com/200/300?image=12
+    #   http://placekitten.com/200/300?image=13
+    #   http://placekitten.com/200/300?image=14
+    #   http://placekitten.com/200/300?image=15
+    #   http://placekitten.com/200/300?image=16
+    # ).sample
   )
 end
 
-10.times {create_robot}
+6.times do |i|
+  create_robot!(i)
+end
