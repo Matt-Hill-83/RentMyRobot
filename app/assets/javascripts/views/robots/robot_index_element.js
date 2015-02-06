1c3a80fa23ec4fa9876cc5e52c2889edc259e4cf
KletusVandamme <matt.hill.email@gmail.com>
2015-02-06 15:12:49 -0800

    index robots link to their show pages

--- app/assets/javascripts/views/robots/robot_index_element.js
+++ app/assets/javascripts/views/robots/robot_index_element.js
@@ -2,12 +2,12 @@ RentMyRobot.Views.RobotIndexElement = Backbone.CompositeView.extend({
   className: 'clearfix',
 
   template: JST['robots/index_element'],
+  tagName: 'li',
 
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
   },
 
-  tagName: 'li',
   render: function () {
     if (!this.model.get('name')){ //qqq why am I checking for the name?
       return this;
@@ -18,16 +18,6 @@ RentMyRobot.Views.RobotIndexElement = Backbone.CompositeView.extend({
     });
 
     this.$el.html(content);
-
-    if (this.model.comments()){
-        // add nested comments
-        var that = this;
-        this.model.comments().each(function (comment) {
-          var view = new RentMyRobot.Views.CommentShow({ model: comment });
-          that.$el.append(view.render().$el);
-        });
-     }
-
 
       return this;
   },
