d9211617cb96a76bfca5f384e31332246a86e3c0
KletusVandamme <matt.hill.email@gmail.com>
2015-02-08 17:16:57 -0800

    cosmetic stuff

--- app/assets/javascripts/views/robots/main.js
+++ app/assets/javascripts/views/robots/main.js
@@ -6,7 +6,6 @@ RentMyRobot.Views.Main = Backbone.CompositeView.extend({
   initialize: function () {
     this.listenTo(this.collection, 'sync', this.render);
     this.addSidebar();
-    this.addMap();
   },
 
   addSidebar: function () {
@@ -16,42 +15,25 @@ RentMyRobot.Views.Main = Backbone.CompositeView.extend({
     this.addSubview('#sidebar-container', sideBarView);
   },
 
-
   addMap: function () {
-
     mapView = new RentMyRobot.Views.MarkerMapShow({
       collection: RentMyRobot.Collections.robots
     });
-    // debugger
-    // this.addSubview('#map-container', mapView);
+    // this.addSubview('#map-container', mapView.$el);
   },
-    //
-    // // this.$el.html(map.$el);
-    // this.$el.find('#map-canvas2').html(map.$el);
-    // map.render();
 
   render: function () {
-
-
     var content = this.template({
       collection: RentMyRobot.Collections.robots
-    } ); // grab the template
+    } );
 
-    this.$el.html(content); // stick the template content into this view's $el
+    this.$el.html(content);
     this.attachSubviews();
 
-
-
-
-
-    // add Google Map
-    map = new RentMyRobot.Views.MarkerMapShow({
-      collection: RentMyRobot.Collections.robots
-    });
-    // this.$el.html(map.$el);
-    debugger
-    this.$el.find('#map-container').html(map.$el);
-    map.render();
+    //qqq how do I put my map into a subview?
+    this.addMap();
+    this.$el.find('#map-container').html(mapView.$el);
+    mapView.render();
 
     return this;
   }
