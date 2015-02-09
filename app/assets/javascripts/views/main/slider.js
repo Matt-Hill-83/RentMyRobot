26196bf1be3e5d661c219a5275fcaef9c78416f9
KletusVandamme <matt.hill.email@gmail.com>
2015-02-08 19:18:17 -0800

    minor changes

--- app/assets/javascripts/views/main/slider.js
+++ app/assets/javascripts/views/main/slider.js
@@ -5,31 +5,26 @@ RentMyRobot.Views.Slider = Backbone.CompositeView.extend({
     this.listenTo(this.collection, 'sync', this.render); //qqq do I need this at this conatiner level?
   },
 
-//
-// <p>
-//   <label for="amount">Price range:</label>
-//   <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
-// </p>
-//
-// <div id="slider-range"></div>
-
-  render: function () {
-
-    var content = this.template({
-      collection: RentMyRobot.Collections.robots
-    });
-    this.$el.html(content);
+  setupSlider: function () {
     // add slider
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
       max: 500,
       values: [ 75, 300 ],
-      // slide: function( event, ui ) {
-      //   $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      // }
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
     });
-    // this.attachSubviews();
+
+  },
+
+  render: function () {
+    var content = this.template({
+      collection: RentMyRobot.Collections.robots
+    });
+    this.$el.html(content);
+    this.setupSlider();
     return this;
   }
 });
