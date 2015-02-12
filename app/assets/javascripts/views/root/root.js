349d6d8bdb45ffc674d18ec15fd3c6c4be8dd5cb
KletusVandamme <matt.hill.email@gmail.com>
2015-02-12 02:17:43 -0800

    modal window works

--- app/assets/javascripts/views/root/root.js
+++ app/assets/javascripts/views/root/root.js
@@ -2,29 +2,19 @@ RentMyRobot.Views.Root = Backbone.CompositeView.extend({
 	template: JST['root/root'],
 
 	initialize: function() {
-		this.locationSelected = false;
 	},
 
 	events: {
-		// "submit form.form-search": "submitSearch",
-		"click .root-banner-image": "searchByCity"
+		"click .root-banner-image": "goToMain"
 	},
 
-	searchByCity: function(event){
+	goToMain: function(event){
 		Backbone.history.navigate("main", {trigger: true});
 	},
 
 	render: function() {
 		var content = this.template();
 		this.$el.html(content);
-		// this.addNavbar();
-
 		return this;
 	},
-
-	addNavbar: function() {
-		var navbarView = new ScareBnb.Views.RootNavbar();
-		this.addSubview(".navbar-vw", navbarView);
-	},
-
 });
