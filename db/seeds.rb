de44aaf4703310c0ded2a02be3b6f2cb4c68a9c2
KletusVandamme <matt.hill.email@gmail.com>
2015-02-15 12:49:42 -0800

    seed working better

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,33 @@
-def create_robot!(i)
-  require 'csv'
+require 'csv'
 
-  csv_data = CSV.read 'robot_seed_data.csv'
-  headers = csv_data.shift.map {|i| i.to_s }
-  string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
-  array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
+csv_data = CSV.read 'robot_seed_data.csv'
+headers = csv_data.shift.map {|i| i.to_s }
+string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
+array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
 
-  # p array_of_hashes
-  #
-  array_of_hashes.each do |row|
-  #   puts "=============="
-  #   puts row
-  #   puts "=============="
-  Robot.create!(
-    name: row['name'],
-    manufacturer: row['manufacturer'],
-    video_url: 'wE3fmFTtP9g ',
-    video_url: row['video_url'],
+def create_robots!(array_of_hashes)
+  array_of_hashes.each_with_index do |row, robot_number|
+    Robot.create!(
+      name: row['name'],
+      manufacturer: row['manufacturer'],
+      video_url: row['video_url'],
+      image_url: row['image_url'],
+      lat: row['lat'].to_i,
+      lng: row['lng'].to_i,
+      price: row['price'].to_i,
+      description: row['description'],
+      robot_type: row['robot_type'],
+      )
 
-    image_url: row['image_url'],
-    lat: row['lat'].to_i,
-    lng: row['lng'].to_i,
-    price: row['price'].to_i,
-    description: row['description'],
-    robot_type: row['robot_type'],
-    )
-  end 
+    numComments = 5
+    numComments.times do |commentNumber|
+      create_comment!(robot_number, commentNumber)
+    end
+  end
 end
 
 def create_comment!(robotNumber, commentNumber)
   comment_text = "This is a fantastic robot.  We took it out for a test on Saturday and had a ball!"
-  description_text = "This robot has been designed to handle all your robotic needs.  No task is too small."
   Comment.create!(
     user_id: 0,
     robot_id: robotNumber,
@@ -39,13 +36,4 @@ def create_comment!(robotNumber, commentNumber)
   )
 end
 
-numRobots = 17
-numComments = 5
-
-numRobots.times do |robotNumber|
-  create_robot!(robotNumber)
-
-  numComments.times do |commentNumber|
-    create_comment!(robotNumber, commentNumber)
-  end
-end
+create_robots!(array_of_hashes)
